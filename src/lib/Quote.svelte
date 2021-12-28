4148dd1b18b4e7a2588635e9a5e0afb3ed1459e7
inklingboi <inklingboiii1@gmail.com>
2021-12-28 18:39:14 +0100

    attempted to fetch random inspirational quote

---
+++ src/lib/Quote.svelte
@@ -0,0 +1,20 @@
+<script>
+    // Fetch quote
+    const api_url = 'https://zenquotes.io/api/quotes/';
+    let quotes = [];
+
+    async function getQuotes(url) {
+        const result = await fetch(url, {
+        'Access-Control-Allow-Origin' : '*',
+        'Access-Control-Allow-Methods':'GET,PUT,POST,DELETE,PATCH,OPTIONS',
+        });
+        const data = await result.json();
+        console.log(JSON.parse(JSON.stringify(data)))
+        return data;
+    }
+
+    quotes = getQuotes(api_url);
+    console.log(quotes)
+</script>
+
+{quotes[0].h}
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,8 @@
 <script>
     import MainHeading from '$lib/MainHeading.svelte';
+    import Quote from '$lib/Quote.svelte';
 </script>
-
-<MainHeading>Generic todo-list</MainHeading>
+<header>
+    <MainHeading>Generic todo-list</MainHeading>
+    <Quote />
+</header>
