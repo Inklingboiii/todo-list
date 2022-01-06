410fe6535653a8228f0e6bdea8a408f7ad47fa28
inklingboi <inklingboiii1@gmail.com>
2022-01-06 15:18:33 +0100

    made it work by adding an endpoint

--- src/lib/Quote.svelte
+++ src/lib/Quote.svelte
@@ -1,26 +1,3 @@
-<script context="module">
-    // Fetch quote
-    // TODO: Fetch multiple quotes at once and cache them externally as described here: https://premium.zenquotes.io/zenquotes-documentation/#call-quotes
-    /** @type {import('@sveltejs/kit').Load} */
-    export async function loadQuote({fetch}) {
-        const api_url = 'https://zenquotes.io/api/random';
-        const result = await fetch(api_url);
-        const quotes = await result.json();
-
-        if(result.ok) {
-            return {
-                props: {
-                    quote: quotes[0]
-                }
-            }
-        }
-
-        return {
-			status: res.status,
-			error: new Error(`Could not load ${url}`)
-		};
-    }
-</script>
 <script>
     export let quote;
 </script>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,10 +1,23 @@
 <script>
     import MainHeading from '$lib/MainHeading.svelte';
-    import Quote, {loadQuote} from '$lib/Quote.svelte';
+    import Quote from '$lib/Quote.svelte';
     export let quote;
 </script>
 <script context="module">
-   export const load = loadQuote;
+    // Fetch quote
+    // TODO: Fetch multiple quotes at once and cache them externally as described here: https://premium.zenquotes.io/zenquotes-documentation/#call-quotes
+    /** @type {import('@sveltejs/kit').Load} */
+
+   export async function load({fetch}) {
+    const result = await fetch('./quote.json');
+    const quote = await result.json();
+    
+        return {
+           props: {
+               quote
+           }
+        }
+   }
 </script>
 <header>
     <MainHeading>Generic to-do list</MainHeading>
