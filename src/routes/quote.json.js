701096fd3f479c51933cecc34525ccd2fb6080a0
inklingboi <inklingboiii1@gmail.com>
2022-01-08 12:54:26 +0100

    forgot to add quote.json

---
+++ src/routes/quote.json.js
@@ -0,0 +1,15 @@
+export async function get() {
+    const api_url = 'https://zenquotes.io/api/random';
+    const result = await fetch(api_url);
+    const quotes = await result.json();
+    if(result.ok) {
+        return {
+           body: quotes[0]
+        }
+    }
+
+    return {
+        status: res.status,
+        error: new Error(`Could not load ${url}`)
+    };
+}
