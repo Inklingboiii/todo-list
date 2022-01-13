f8b594c23a99ff0f919b0c56ffd3ab7d5a15617b
Inklingboi <inklingboiii1@gmail.com>
2022-01-13 23:08:08 +0100

    forgot to stage files

---
+++ src/lib/TodosCard.svelte
@@ -0,0 +1,12 @@
+<script>
+	import todos from '$lib/todosStore';
+	$: dailyTodos = $todos.filter((todo) => todo.today);
+</script>
+
+{#if dailyTodos.length > 0}
+	{#each dailyTodos as dailyTodo}
+		<p>{dailyTodo.text}</p>
+	{/each}
+{:else}
+	No todos.
+{/if}
