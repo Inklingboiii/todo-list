82bc3f4812be537aeb3337f5bcf4de0bd67c407f
inklingboi <inklingboiii1@gmail.com>
2022-01-29 23:11:36 +0100

    deleted todofunctrions file

--- src/lib/todoFunctions.ts
+++
@@ -1,7 +0,0 @@
-import { todosStore } from '$lib/todosStores';
-
-export function expireTodo(expiredTodo, successfull) {
-	todosStore.filter((todo) => {
-		if(todo.id === expiredTodo.id) true
-	})
-}
