2c5f38f319ed3d9ad4b840c4cf33f2733348fc60
Inklingboi <inklingboiii1@gmail.com>
2022-02-03 22:07:38 +0100

    worked on menu

--- src/lib/KebabMenu.svelte
+++ src/lib/KebabMenu.svelte
@@ -1,11 +1,19 @@
 <script>
     import { fade } from 'svelte/transition';
     let toggle = false;
+    const actions = ['Delete', 'Edit'];
+
+    function handleKeyup(event) {
+        console.log('keyup')
+        if(event.key === 'ArrowDown') {
+            console.log('down')
+        }
+    }
 </script>
 
 <!-- Extremely big brain toggle mechanic -->
 
-<button on:click={() => toggle ^= true} aria-haspopup="true" aria-expanded={toggle ? 'true' : null} aria-controls="menu" id="menu-button">
+<button on:click={() => toggle ^= true} aria-haspopup="true" aria-expanded={toggle ? 'true' : null} aria-controls="menu">
     <svg width="16px" height="16px" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" version="1.1" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5">
         <circle cx="8" cy="2.5" r=".75"/>
         <circle cx="8" cy="8" r=".75"/>
@@ -13,9 +21,11 @@
     </svg>
 </button>
 {#if toggle}
-    <ul id="menu" role="menu" transition:fade>
-        <li role="menuitem">Delete</li>
-    </ul>
+    <div id="menu" role="menu" transition:fade>
+        {#each actions as action, i (i)}
+            <button role="menuitem" tabindex="-1" on:keyup={handleKeyup}>{action}</button>
+        {/each}
+    </div>
 {/if}
 <style>
     button {
@@ -29,7 +39,7 @@
         color: white;
     }
 
-    ul {
+    #menu {
         position: absolute;
         z-index: 1;
         right: 0;
