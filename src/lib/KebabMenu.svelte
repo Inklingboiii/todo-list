2b0d4aabd6a0410cb1b771deb90118006171131a
inklingboi <inklingboiii1@gmail.com>
2022-02-04 19:55:29 +0100

    almost completed kebab menu

--- src/lib/KebabMenu.svelte
+++ src/lib/KebabMenu.svelte
@@ -1,19 +1,32 @@
 <script>
     import { fade } from 'svelte/transition';
+    let menu;
+    let menuButton;
+    let focusedMenuItem = 0;
     let toggle = false;
     const actions = ['Delete', 'Edit'];
 
     function handleKeyup(event) {
-        console.log('keyup')
-        if(event.key === 'ArrowDown') {
-            console.log('down')
-        }
+        console.log('keydown', event.key)
+        let menuItems = [...menu.children];
+        if(event.key === 'Tab' || event.key === 'Escape') toggle = false;
+        if(event.key === 'Escape') menuButton.focus();
+        if(event.key === 'ArrowUp' || event.key === 'ArrowRight') focusedMenuItem--;
+        if(event.key === 'ArrowDown' || event.key === 'ArrowLeft') focusedMenuItem--;
+        menuItems.at(focusedMenuItem % menuItems.length).focus();
+    }
+
+    function handleToggle(element) {
+        console.log('toggle', element)
+        menu = element;
+        focusedMenuItem = 0;
+        menu.children[0].focus();
     }
 </script>
 
 <!-- Extremely big brain toggle mechanic -->
 
-<button on:click={() => toggle ^= true} aria-haspopup="true" aria-expanded={toggle ? 'true' : null} aria-controls="menu">
+<button on:click={() => toggle ^= true} bind:this={menuButton} aria-haspopup="true" aria-expanded={toggle ? 'true' : null} aria-controls="menu" id="menu-button">
     <svg width="16px" height="16px" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" version="1.1" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5">
         <circle cx="8" cy="2.5" r=".75"/>
         <circle cx="8" cy="8" r=".75"/>
@@ -21,9 +34,9 @@
     </svg>
 </button>
 {#if toggle}
-    <div id="menu" role="menu" transition:fade>
+    <div id="menu" role="menu" aira-labelledby="menu-button" use:handleToggle transition:fade>
         {#each actions as action, i (i)}
-            <button role="menuitem" tabindex="-1" on:keyup={handleKeyup}>{action}</button>
+            <button role="menuitem" tabindex="-1" on:keydown={handleKeyup}>{action}</button>
         {/each}
     </div>
 {/if}
@@ -33,7 +46,9 @@
         padding: 0;
         background-color: transparent;
         cursor: pointer;
+        color: white;
     }
+
 
     svg {
         color: white;
@@ -44,7 +59,18 @@
         z-index: 1;
         right: 0;
         top: 2.5em;
-        background: #333;
+        display: flex;
+        flex-direction: column;
+        gap: 1em;
+        background-color: hsl(0, 0%, 20%);
+    }
+
+    #menu > button {
         padding: 1em;
+        transition: background-color .3s;
+    }
+
+    #menu > button:is(:focus, :hover) {
+        background-color: hsl(0, 0%, 50%, 0.75);
     }
 </style>
