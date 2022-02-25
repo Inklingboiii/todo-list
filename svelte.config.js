1cd2b7cc0a8f05f518efa9d8d2e434c6ac6b4a99
inklingboi <inklingboiii1@gmail.com>
2022-02-25 19:01:33 +0100

    try fixing deployment issues

--- svelte.config.js
+++ svelte.config.js
@@ -5,6 +5,9 @@ import adapter from '@sveltejs/adapter-vercel';
 const config = {
 	kit: {
 		adapter: adapter(),
+
+		// hydrate the <div id="svelte"> element in src/app.html
+		target: '#svelte'
 	}
 };
 
