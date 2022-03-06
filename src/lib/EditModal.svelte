84efb7b2db1cd7e8593d44559a1c86a2789c3aa8
inklingboi <inklingboiii1@gmail.com>
2022-03-06 15:46:56 +0100

    trapped the tab key in the modal

--- src/lib/EditModal.svelte
+++ src/lib/EditModal.svelte
@@ -2,9 +2,13 @@
     export let todo;
     export let closeEditModal;
     import { editTodo } from '$lib/todosStores';
+    let firstTabElement;
+    let lastTabElement;
 
     function modal(modalElement) {
-        modalElement.querySelector('input').focus();
+        firstTabElement = modalElement.querySelector('#first')
+        firstTabElement.focus();
+        lastTabElement = modalElement.querySelector('#last');
     }
 
     function handleSubmit() {
@@ -14,14 +18,36 @@
 
     function handleKeydown(event) {
         if(event.key === 'Escape') closeEditModal();
+        trapTabKey(event);
+    }
+
+    // https://github.com/udacity/ud891/blob/gh-pages/lesson2-focus/07-modals-and-keyboard-traps/solution/modal.js
+    function trapTabKey(event) {
+        if(event.key === 'Tab') {
+            
+            // Shift + Tab => Backwards tab
+            if(event.shiftKey) {
+                if(document.activeElement === firstTabElement) {
+                    event.preventDefault();
+                    lastTabElement.focus();
+                }
+
+            // Tab
+            } else {
+                if(document.activeElement === lastTabElement) {
+                    event.preventDefault();
+                    firstTabElement.focus();
+                }
+            }
+        }
     }
 </script>
 
 <form role="dialog" aria-modal="true" aria-labelledby="heading" on:submit|preventDefault={handleSubmit} on:keydown={handleKeydown} use:modal>
     <h3 id="heading">Edit Todo</h3>
-    <input bind:value={todo.text}>
+    <input bind:value={todo.text} id="first">
     <button>Save</button>
-    <button type="button" on:click={closeEditModal}>Cancel</button>
+    <button type="button" on:click={closeEditModal} id="last">Cancel</button>
 </form>
 <div class="overlay"></div>
 
@@ -38,5 +64,8 @@
         inset: 0;
         background: var(--color-dark);
         z-index: 1;
+    }
+    h3 {
+        color:black;
     }
 </style>
