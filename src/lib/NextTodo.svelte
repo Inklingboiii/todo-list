f53f2a6b38153fd11925d64cec686edb0805a3fa
Inklingboi <inklingboiii1@gmail.com>
2022-03-24 20:23:31 +0100

    formatted next goal TODO: make it so the code doesnt get staright up copied from the tododeadline.svelte

--- src/lib/NextTodo.svelte
+++ src/lib/NextTodo.svelte
@@ -1,12 +1,36 @@
 <script>
+    import { onMount } from 'svelte';
     import { todosStore, sortTodosByDeadline } from '$lib/todosStores';
     $: nextTodo = sortTodosByDeadline($todosStore.filter((todo) => !todo.today))[0];
+    $: deadline = new Intl.DateTimeFormat('default', {
+          hour: 'numeric',
+          year: 'numeric',
+          month: 'numeric',
+          day: 'numeric',
+          minute: 'numeric',
+          'second': 'numeric'
+        }).format(nextTodo.deadline);
+    let currentTime = Date.now();
+    const lateLimit = 1000 * 60 * 60 * 24 // A day
+
+    onMount(() => {
+        let deadlineInterval = setInterval(() => {
+            currentTime = Date.now();
+            // Check if todo expired
+            if(nextTodo.deadline <= currentTime) {
+                clearInterval(deadlineInterval);
+                expireTodo(nextTodo, false);
+            }
+        }, 1000)
+    });
 </script>
 
 <article>
     <h2>Next long term goal</h2>
     <p>{nextTodo.text}</p>
-    <time>{nextTodo.deadline}</time>
+    <time class:late={nextTodo.deadline - currentTime < lateLimit}>
+        {deadline}
+    </time>
 </article>
 
 <style>
@@ -15,5 +39,10 @@
         flex-direction: column;
         align-items: center;
         text-align: center;
+    }
+
+    .late {
+        color: var(--color-primary);
+        font-weight: bold;
     }
 </style>
--- src/lib/todosStores.ts
+++ src/lib/todosStores.ts
@@ -15,9 +15,9 @@ type InactiveTodos = Todos & Array<{
 
 // Using test data for now, will fetch actual data later
 const todos: Todos = [
-	{text: 'example test', today: true, deadline: new Date(new Date().setHours(new Date().getHours() + 2)), id: uuid()},
+	{text: 'example test', today: true, deadline: new Date(new Date(new Date().setHours(new Date().getHours() + 1)).setSeconds(new Date().getSeconds() + 10)), id: uuid()},
 	{text: 'example test 2', today: true, deadline: new Date(new Date().setSeconds(new Date().getSeconds() + 120)), id: uuid()},
-	{text: 'example goal 1', today: false, deadline: new Date(new Date().setHours(new Date().getHours() + 48)), id: uuid()},
+	{text: 'example goal 1', today: false, deadline: new Date(new Date(new Date().setHours(new Date().getHours() + 24)).setSeconds(new Date().getSeconds() + 10)), id: uuid()},
 	{text: 'example goal 2', today: false, deadline: new Date(new Date().setMonth(new Date().getMonth() + 1)), id: uuid()},
 
 ];
