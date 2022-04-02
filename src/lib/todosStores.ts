1f3161a3c72664a05299906996a6cef14b9e7ec7
inklingboi <inklingboiii1@gmail.com>
2022-04-02 09:40:12 +0200

    fixed bug by only storing number as deadline, and also changed the todo types

--- src/lib/todosStores.ts
+++ src/lib/todosStores.ts
@@ -1,27 +1,30 @@
 import { writable, get } from 'svelte/store';
 import { nanoid as uuid } from 'nanoid'
 
-type Todos = Array<{
+type Todo = {
 	text: string,
 	today: boolean,
-	deadline: Date,
+	deadline: number,
 	id: string
-}>
+}
 
-type InactiveTodos = Todos & Array<{
+type InactiveTodo = Todo & {
 	succeeded: boolean
-}>
+}
+
+type Todos = Array<Todo>;
+type InactiveTodos = Array<InactiveTodo>;
 	
 
 // Using test data for now, will fetch actual data later
 const todos: Todos = [
-	{text: 'example test', today: true, deadline: new Date(new Date(new Date().setHours(new Date().getHours() + 1)).setSeconds(new Date().getSeconds() + 10)), id: uuid()},
-	{text: 'example test 2', today: true, deadline: new Date(new Date().setSeconds(new Date().getSeconds() + 120)), id: uuid()},
-	{text: 'example goal 1', today: false, deadline: new Date(new Date(new Date().setHours(new Date().getHours() + 24)).setSeconds(new Date().getSeconds() + 10)), id: uuid()},
-	{text: 'example goal 2', today: false, deadline: new Date(new Date().setMonth(new Date().getMonth() + 1)), id: uuid()},
+	{text: 'example test', today: true, deadline: new Date(new Date(new Date().setHours(new Date().getHours() + 1)).setSeconds(new Date().getSeconds() + 10)).getTime(), id: uuid()},
+	{text: 'example test 2', today: true, deadline: new Date(new Date().setSeconds(new Date().getSeconds() + 120)).getTime(), id: uuid()},
+	{text: 'example goal 1', today: false, deadline: new Date(new Date(new Date().setHours(new Date().getHours() + 24)).setSeconds(new Date().getSeconds() + 10)).getTime(), id: uuid()},
+	{text: 'example goal 2', today: false, deadline: new Date(new Date().setMonth(new Date().getMonth() + 1)).getTime(), id: uuid()},
 
 ];
-const inactiveTodos: InactiveTodos = [{text: 'missed todo', today: true, deadline: new Date(), succeeded: false, id: uuid()}];
+const inactiveTodos: InactiveTodos = [{text: 'missed todo', today: true, deadline: new Date().getTime(), succeeded: false, id: uuid()}];
 
 const todosStore = writable(todos);
 todosStore.subscribe((todos) => {
@@ -32,23 +35,21 @@ inactiveTodosStore.subscribe((inactiveTodos) => {
 	// Update database everytime store changes
 });
 
-function addTodo(todo: Todos[0]) {
-console.log(JSON.parse(JSON.stringify(get(todosStore))));
-todosStore.update((todos) => [...todos, todo])
-console.log(JSON.parse(JSON.stringify(get(todosStore))));
+function addTodo(todo: Todo) {
+	todosStore.update((todos) => [...todos, todo])
 }
 
-function expireTodo(todo: Todos[0], succeeded: boolean) {
+function expireTodo(todo: Todo, succeeded: boolean) {
 	// Move todo to inactivetodo store
 	todosStore.update((todos) => todos.filter((storeTodo) => storeTodo.id !== todo.id));
 	inactiveTodosStore.update((storeTodos) => [...storeTodos, {...todo, succeeded: succeeded}]);
 }
 
-function deleteTodo(todo: Todos[0]) {
+function deleteTodo(todo: Todo) {
 	todosStore.update((todos) => todos.filter((storeTodo) => storeTodo.id !== todo.id));
 }
 
-function editTodo(todo: Todos[0]) {
+function editTodo(todo: Todo) {
 	todosStore.update((todos) => {
 		todos.find((storeTodo) => 
 		storeTodo.id === todo.id).text = todo.text;
@@ -58,9 +59,7 @@ function editTodo(todo: Todos[0]) {
 
 function sortTodosByDeadline(todosStore: Todos) {
 	// Make a copy of it, since sort() doesn't return a new array
-	return todosStore.slice().sort((todoOne: Todos[0], todoTwo: Todos[0]) => {
-		return todoOne.deadline.getTime() - todoTwo.deadline.getTime()
-	});
+	return todosStore.slice().sort((todoOne: Todo, todoTwo: Todo) => todoOne.deadline - todoTwo.deadline);
 }
 
 export { todosStore, inactiveTodosStore, addTodo, expireTodo, deleteTodo, editTodo, sortTodosByDeadline };
