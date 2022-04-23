9ea7b095b33b8e759a66f9208a08a031d3487313
inklingboi <inklingboiii1@gmail.com>
2022-04-23 23:56:20 +0200

    set the icons to the according dates

--- src/lib/Calendar/CalendarCell.svelte
+++ src/lib/Calendar/CalendarCell.svelte
@@ -7,6 +7,9 @@
     import GoalIcon from '$lib/icons/GoalIcon.svelte';
 
     const focus = el => el.focus();
+    // Check for succeeded, so it only checks for active todos
+    $: hasDailyTodos = todos.some((todo) => todo.today && todo.succeeded === undefined);
+    $: hasGoals = todos.some((todo) => !todo.today);
 </script>
 {#if isCurrentDay}
 <td tabindex="0"  class="current-day" role="gridcell" aria-selected={true} use:focus>
@@ -14,8 +17,12 @@
     {#each todos as todo}
     <p>{todo.text}</p>
     {/each}
-    <DailyTodosIcon />
-    <GoalIcon />
+    {#if hasDailyTodos}
+        <DailyTodosIcon />
+    {/if}
+    {#if hasGoals}
+        <GoalIcon />
+    {/if}
 </td>
 {:else}
 <td tabindex="-1" class:not-in-month={!inMonth}>
@@ -23,6 +30,12 @@
     {#each todos as todo}
     <p>{todo.text}</p>
     {/each}
+    {#if hasDailyTodos}
+        <DailyTodosIcon />
+    {/if}
+    {#if hasGoals}
+        <GoalIcon />    
+    {/if}
 </td>
 {/if}
 
