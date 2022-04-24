4ef36af0df5aa8c2d11f5e3f08dea5a02e623d68
inklingboi <inklingboiii1@gmail.com>
2022-04-24 14:58:49 +0200

    cleaned up calendar container by moving it to a seperate file

--- src/lib/Calendar/CalendarCell.svelte
+++ src/lib/Calendar/CalendarCell.svelte
@@ -3,16 +3,16 @@
     export let inMonth;
     export let isCurrentDay;
     export let todos;
+    import CalendarCellContainer from './CalendarCellContainer.svelte';
     import DailyTodosIcon from '$lib/icons/DailyTodosIcon.svelte';
     import GoalIcon from '$lib/icons/GoalIcon.svelte';
 
-    const focus = el => el.focus();
     // Check for succeeded, so it only checks for active todos
     $: hasDailyTodos = todos.some((todo) => todo.today && todo.succeeded === undefined);
     $: hasGoals = todos.some((todo) => !todo.today);
 </script>
-{#if isCurrentDay}
-<td tabindex="0"  class="current-day" role="gridcell" aria-selected={true} use:focus>
+
+<CalendarCellContainer {isCurrentDay} {inMonth}>
     <h2>{day}</h2>
     {#each todos as todo}
     <p>{todo.text}</p>
@@ -23,39 +23,5 @@
     {#if hasGoals}
         <GoalIcon />
     {/if}
-</td>
-{:else}
-<td tabindex="-1" class:not-in-month={!inMonth}>
-    <h2>{day}</h2>
-    {#each todos as todo}
-    <p>{todo.text}</p>
-    {/each}
-    {#if hasDailyTodos}
-        <DailyTodosIcon />
-    {/if}
-    {#if hasGoals}
-        <GoalIcon />    
-    {/if}
-</td>
-{/if}
+</CalendarCellContainer>
 
-
-<style>
-    td {
-        border: 1px solid black;
-        text-align: center;
-        cursor: pointer;
-    }
-
-    td:focus-within {
-        outline: 3px solid black;
-    }
-
-    .not-in-month {
-        background: gray
-    }
-
-    .current-day {
-        background: var(--color-primary);
-    }
-</style>
---
+++ src/lib/Calendar/CalendarCellContainer.svelte
@@ -0,0 +1,36 @@
+<script>
+    export let isCurrentDay;
+    export let inMonth;
+    const focus = el => el.focus();
+    console.log(isCurrentDay, inMonth)
+</script>
+
+{#if isCurrentDay}
+    <td tabindex="0"  class="current-day" role="gridcell" aria-selected={true} use:focus>
+        <slot></slot>
+    </td>
+{:else}
+    <td tabindex="-1" class:not-in-month={!inMonth}>
+        <slot></slot>
+    </td>
+{/if}
+
+<style>
+    td {
+        border: 1px solid black;
+        text-align: center;
+        cursor: pointer;
+    }
+
+    td:hover {
+        outline: 3px solid black;
+    }
+
+    .not-in-month {
+        background: var(--color-gray);
+    }
+
+    .current-day {
+        background: var(--color-primary);
+    }
+</style>
