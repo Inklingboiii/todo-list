8111f7027dbaefe6519b5231ddf112be9224701b
inklingboi <inklingboiii1@gmail.com>
2022-04-29 22:55:44 +0200

    decoupled modal compnent to reuse it for calendar

--- src/lib/utilities/EditModal.svelte
+++ src/lib/utilities/EditModal.svelte
@@ -1,18 +1,10 @@
 <script>
-    export let todo;
     export let closeEditModal;
+    export let todo;
     import { editTodo } from '$lib/todosStores';
-    import { fly } from 'svelte/transition';
+    import Modal from '$lib/utilities/Modal.svelte';
     import Button from '$lib/utilities/Button.svelte';
-    let firstTabElement;
-    let lastTabElement;
     let inputValue = todo.text;
-
-    function modal(modalElement) {
-        firstTabElement = modalElement.querySelector('#first')
-        firstTabElement.focus();
-        lastTabElement = modalElement.querySelector('#last');
-    }
 
     function handleSubmit() {
         todo.text = inputValue;
@@ -20,43 +12,17 @@
         closeEditModal();
     }
 
-    function handleKeydown(event) {
-        if(event.key === 'Escape') closeEditModal();
-        trapTabKey(event);
-    }
-
-    // https://github.com/udacity/ud891/blob/gh-pages/lesson2-focus/07-modals-and-keyboard-traps/solution/modal.js
-    function trapTabKey(event) {
-        if(event.key === 'Tab') {
-            
-            // Shift + Tab => Backwards tab
-            if(event.shiftKey) {
-                if(document.activeElement === firstTabElement) {
-                    event.preventDefault();
-                    lastTabElement.focus();
-                }
-
-            // Tab
-            } else {
-                if(document.activeElement === lastTabElement) {
-                    event.preventDefault();
-                    firstTabElement.focus();
-                }
-            }
-        }
-    }
 </script>
-
-<form role="dialog" aria-modal="true" aria-labelledby="heading" on:submit|preventDefault={handleSubmit} on:keydown={handleKeydown} use:modal transition:fly={{y: 500, duration: 300}}>
-    <h3 id="heading">Edit Todo</h3>
-    <input bind:value={inputValue} id="first">
-    <div>
-        <Button>Save</Button>
-        <Button minimal type="button" onClick={closeEditModal} id="last">Cancel</Button>
-    </div>
-</form>
-<!-- Used as tint and to close modal on focusout -->
-<div class="overlay" on:click={closeEditModal}></div>
+<Modal {closeEditModal}>
+    <h3 id="heading" >Edit Todo</h3>
+    <form on:submit|preventDefault={handleSubmit}>
+        <input bind:value={inputValue} id="first">
+        <div>
+            <Button>Save</Button>
+            <Button minimal type="button" onClick={closeEditModal} id="last">Cancel</Button>
+        </div>
+    </form>
+</Modal>
 
 <style>
     form {
@@ -65,29 +31,13 @@
         align-items: flex-start;
         justify-content: space-around;
         gap: 1em;
-        position: fixed;
-        inset: 0;
-        max-height: calc(10% + 5rem);
-        max-width: calc(30% + 5rem);
-        padding: 1em;
-        margin: auto;
-        background: white;
-        color: black;
-        z-index: 10;
-    }
-
-   .overlay {
-        position: fixed;
-        inset: 0;
-        background: var(--color-dark);
-        z-index: 1;
     }
     
     input {
         width: 100%;
     }
 
-    form > div {
+    div {
         display: flex;
         gap: 1em;
         align-self: flex-end;
---
+++ src/lib/utilities/Modal.svelte
@@ -0,0 +1,66 @@
+<script>
+    export let closeEditModal;
+    import { fly } from 'svelte/transition';
+    let firstTabElement;
+    let lastTabElement;
+
+    function modal(modalElement) {
+        firstTabElement = modalElement.querySelector('#first')
+        firstTabElement.focus();
+        lastTabElement = modalElement.querySelector('#last');
+    }
+
+
+    function handleKeydown(event) {
+        if(event.key === 'Escape') closeEditModal();
+        trapTabKey(event);
+    }
+
+    // https://github.com/udacity/ud891/blob/gh-pages/lesson2-focus/07-modals-and-keyboard-traps/solution/modal.js
+    function trapTabKey(event) {
+        if(event.key === 'Tab') {
+            
+            // Shift + Tab => Backwards tab
+            if(event.shiftKey) {
+                if(document.activeElement === firstTabElement) {
+                    event.preventDefault();
+                    lastTabElement.focus();
+                }
+
+            // Tab
+            } else {
+                if(document.activeElement === lastTabElement) {
+                    event.preventDefault();
+                    firstTabElement.focus();
+                }
+            }
+        }
+    }
+</script>
+
+<div role="dialog" aria-modal="true" aria-labelledby="heading" on:keydown={handleKeydown} use:modal transition:fly={{y: 500, duration: 300}}>
+    <slot></slot>
+</div>
+<!-- Used as tint and to close modal on focusout -->
+<div class="overlay" on:click={closeEditModal}></div>
+
+<style>
+    div[role="dialog"] {
+        position: fixed;
+        inset: 0;
+        max-height: calc(10% + 5rem);
+        max-width: calc(30% + 5rem);
+        padding: 1em;
+        margin: auto;
+        background: white;
+        color: black;
+        z-index: 10;
+    }
+
+   .overlay {
+        position: fixed;
+        inset: 0;
+        background: var(--color-dark);
+        z-index: 1;
+    }
+</style>
