742c8d1ab89c11b6d824e09b1b977953102fdede
Inklingboi <inklingboiii1@gmail.com>
2022-07-12 22:46:52 +0200

    added server side cache that hopefully works

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,15 +1,32 @@
+const cache = {
+    data: null,
+    time: new Date().getTime()
+};
+const timeout = 1000 * 60 * 60 * 5;
 export async function get() {
+    if(cache.time + timeout < new Date().getTime() || cache.data !== null) return {
+        headers: {
+            'cache-control': 'public, max-age=' + timeout
+        },
+        body: {
+            quotes: cache.data
+        }
+    }
+    console.log('new data fetch')
     const api_url = 'https://zenquotes.io/api/quotes/';
     const result = await fetch(api_url);
-    const quotes = await result.json();
     if(result.ok) {
+        const quotes = await result.json();
+        cache.data = quotes
+        
+        cache.time = new Date().getTime();
         return {
-           headers: {
-            'cache-control': 'public, max-age=86400'
-           },
-           body: {
-               quotes
-               }
+            headers: {
+                'cache-control': 'public, max-age=' + timeout
+            },
+            body: {
+                quotes
+            }
         }
     }
 
