248663607b000a2fac444c73c297a6357313ad67
inklingboi <inklingboiii1@gmail.com>
2022-07-17 18:38:34 +0200

    made long term goals have same calendercellmodal behaviour

--- src/lib/Calendar/CalendarModal.svelte
+++ src/lib/Calendar/CalendarModal.svelte
@@ -1,5 +1,6 @@
 <script>
     import Modal from '$lib/utilities/Modal.svelte';
+    import CalendarCellListing from './CalendarCellListing.svelte';
     import Button from '$lib/utilities/Button.svelte';
     import CallToAction from '$lib/utilities/CallToAction.svelte';
     export let closeModal;
@@ -17,36 +18,11 @@
         <div class="todos">
             <div>
                 <h4>Daily todos</h4>
-                {#if dailyTodos.length > 0}
-                    <ul>
-                        {#each dailyTodos as todo}
-                            <li>
-                                {#if todo.succeeded === false}
-                                    <s>{todo.text}</s>
-                                {:else if todo.succeeded === true}
-                                    {todo.text} 
-                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 17.837 17.837" width="16" height="16" style="enable-background:new 0 0 17.837 17.837" xml:space="preserve"><path style="fill:var(--color-primary)" d="M16.145 2.571a.7.7 0 0 0-.99 0L6.92 10.804l-4.241-4.27a.698.698 0 0 0-.989 0L.204 8.019a.703.703 0 0 0 0 .99l6.217 6.258a.704.704 0 0 0 .99 0L17.63 5.047a.7.7 0 0 0 0-.994l-1.485-1.482z"/></svg>
-                                {:else}
-                                    {todo.text}
-                                {/if}
-                            </li>
-                        {/each}
-                    </ul>
-                {:else}
-                    <p>None</p>
-                {/if}
+                <CalendarCellListing todos={dailyTodos} />
             </div>
             <div>
                 <h4>Long term goals</h4>
-                {#if goalTodos.length > 0}
-                    <ul>
-                        {#each goalTodos as todo}
-                            <li>{todo.text}</li>
-                        {/each}
-                    </ul>
-                {:else}
-                    <p>None</p>
-                {/if}
+                <CalendarCellListing todos={goalTodos} />
             </div>
         </div>
         
